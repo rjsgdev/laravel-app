72181c9e78d78e6a407caa055ae1805d8c6d23dc
webdevmas <webdevmas@gmail.com>
2022-01-20 21:14:35 +0800

    Add database migration for employees and companies

---
+++ database/migrations/2022_01_20_130525_create_companies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 255);
+            $table->string('address', 255);
+            $table->string('website_url', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
